c560aa1211a0eceaf854bfb9165cd98249fea252
Oleksii-coder4 <loklom.pp@gmail.com>
2024-09-20 15:15:46 +0300

    add task count logic

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
 const input = document.getElementById("todoInput");
 const todoList = document.getElementById("todoTaskList");
 
+const doneChecker = document.getElementById("doneChecker");
+const totalChecker = document.getElementById("totalChecker");
+const todoChecker = document.getElementById("todoChecker");
+
+const showTasksAmount = function (total = 0, done = 0, todo = 0) {
+  doneChecker.textContent = `Done: ${done}`;
+  totalChecker.textContent = `Total: ${total}`;
+  todoChecker.textContent = `To-do: ${todo}`;
+};
+
+const CheckAndShowTasksAmount = function () {
+  const total = todoList.childElementCount;
+  const done = document.querySelectorAll("li.task-checked").length;
+  const todo = total - done;
+  showTasksAmount(total, done, todo);
+};
+
+function showTasks() {
+  todoList.innerHTML = localStorage.getItem("todoTasks");
+}
+showTasks();
+CheckAndShowTasksAmount();
+
 function addTask(input, todoList) {
   if (input.value.trim() === "") {
     alert("You must write something");
@@ -32,13 +55,8 @@ function checkTask(event) {
 
 function saveData() {
   localStorage.setItem("todoTasks", todoList.innerHTML);
+  CheckAndShowTasksAmount();
 }
-
-function showTasks() {
-  todoList.innerHTML = localStorage.getItem("todoTasks");
-}
-
-showTasks();
 
 todoList.addEventListener("click", checkTask);
 todoList.addEventListener("click", deleteTask);
